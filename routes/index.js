c1f48da799f3e8d49f4f538ecc41399521aaa685
julianocribeiro <julianocorrearibeiro@yahoo.com.br>
2022-08-15 20:26:16 -0300

    css

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 const homeController = require('../controllers/homeController');
+const pagamentoController = require('../controllers/pagamentoController');
 var router = express.Router();
 
 router.get('/', homeController.index);
+router.get('/pagamento', pagamentoController.index);
 
 module.exports = router;
